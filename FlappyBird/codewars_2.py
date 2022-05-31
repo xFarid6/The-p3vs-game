3bcf03b51b1e35385552f7f8115715a8a9ddf12e
xFarid6 <giomagic@outlook.it>
2022-05-31 11:08:56 +0200

    day 2

--- FlappyBird/codewars_2.py
+++ FlappyBird/codewars_2.py
@@ -18,9 +18,91 @@ def scramble(s1, s2):   # letsgooooooo
 
 ################################################################################################
 
-# expressing an ordered list of integers using a comma separated list of either
-# individual integers
-# or a range of integers denoted by the starting integer separated from the end integer in the range by a dash, '-'. 
-# The range includes all integers in the interval including both endpoints. It is not considered a range unless it spans at least 3 numbers. 
+# define a function that takes a list of integers and returns
+# a string with each number separated by a comma and dashes '-' between adjacent numbers
+# e.g. [1, 2, 3, 4, 5] -> '1-5'
+# e.g. [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] -> '1-10'
+def dash_insert(numbers):
+    return '-'.join(str(numbers[i]) + str(numbers[i+1])[0] for i in range(len(numbers)-1))
+
 def solution(l):
-    return ','.join(['-'.join(map(str, range(i, j + 1))) if j - i >= 2 else str(i) for i, j in zip(l, l[1:])])
+    return ','.join(
+        ['-'.join(
+            map(str, range(i, j + 1))) if j - i >= 2 
+            else str(i) for i, j in zip(l, l[1:])])
+         
+def detect_range(input_list):
+    start = None
+    length = 0
+
+    for elem in input_list:
+
+        # First element
+        if start is None:
+            start = elem
+            length = 1
+            continue
+
+        # Element in row, just count up
+        if elem == start + length:
+            length += 1
+            continue
+
+        # Otherwise, yield
+        if length == 1:
+            yield start
+        else:
+            yield (start, start+length)
+
+        start = elem
+        length = 1
+
+    if length == 1:
+        yield start
+    else:
+        yield (start, start+length)
+
+a = [1, 2, 3, 4, 6, 7, 8, 9, 10]
+print(list(detect_range(a)))
+def solution(l):
+    ranges = list(detect_range(l))
+    print(ranges)
+    sol = ''
+    for e in ranges:
+        if isinstance(e, int): sol += str(e) + ','
+        elif e[0] == e[1] - 2:
+            sol += str(e[0]) + ',' + str(e[0]+1) + ','
+        else:
+            sol += str(e[0]) +'-'+str(e[1]-1)+','
+            
+    return sol.rstrip(',')
+
+# a well made one
+def solution(arr):
+    ranges = []
+    a = b = arr[0]
+    for n in arr[1:] + [None]:
+        if n != b+1:
+            ranges.append(str(a) if a == b else "{}{}{}".format(a, "," if a+1 == b else "-", b))
+            a = n
+        b = n
+    return ",".join(ranges)
+
+
+######################################################################################
+
+# The Pied Piper has been enlisted to play his magical tune and coax all the rats out of town.
+# But some of the rats are deaf and are going the wrong way!
+# How many deaf rats are there?
+# Legend
+# P = The Pied Piper
+# O~ = Rat going left
+# ~O = Rat going right
+# Example
+# ex1 ~O~O~O~O P has 0 deaf rats
+# ex2 P O~ O~ ~O O~ has 1 deaf rat
+# ex3 ~O~O~O~OP~O~OO~ has 2 deaf rats
+# write a function to count O~ before P and ~O after P
+def deaf_rats(n):
+    return n.count('~O') - n.count('O~') 
+    
